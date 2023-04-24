388710fea94cb4309bf6903394b7cff5699ae3ab
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2023-04-24 14:46:04 +0200

    MealItem Done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,13 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span></span>
+      <span>Your Cart</span>
       <span className={classes.badge}>3</span>
     </button>
   );
