bd765d301745ecccac2a1a3e4e6f7d36e7e76f50
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2023-05-10 14:06:02 +0200

    Fetching data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,17 +1,35 @@
 import Card from "../UI/Card";
 import MealItem from "./MealsItem/MealItem";
 import classes from "./Available.module.css";
-
-const DUMMY_MEALS = async () => {
-  const response = await fetch(
-    "https://react-http-129a6-default-rtdb.firebaseio.com/meals.json",
-  );
-  const data = response.json();
-  console.log(data);
-};
+import { useEffect, useState } from "react";
 
 const AvialableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-129a6-default-rtdb.firebaseio.com/meals.json",
+      );
+      const responseData = await response.json();
+
+      const loadedData = [];
+
+      for (const key in responseData) {
+        loadedData.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedData);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
---
+++ src/hooks/use-http.js
@@ -0,0 +1,5 @@
+const useHttp = () => {
+  fetch("https://react-http-129a6-default-rtdb.firebaseio.com/meals.json");
+};
+
+export default useHttp;
